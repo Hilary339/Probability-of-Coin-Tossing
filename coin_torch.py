cc0c8d544bde02e96ea249c9c6e1d8ad3add1fd3
Hilary339 <3088165253@qq.com>
2026-09-05 18:57:50 +0800

    add comment
    Add simulation for coin toss probabilities and frequencies.

--- coin_torch.py
+++ coin_torch.py
@@ -2,9 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 N = 100000
+#构建正反面的概率
 fair_probs=torch.tensor([0.5,0.5])
+#进行N次试验，replacement=True表示拿过的还可以再拿
 samples=torch.multinomial(fair_probs,N,replacement=True)
+#累计正面朝上的次数
 cum_heads=(samples==0).cumsum(dim=0)
+#正面朝上的频率
 head_frqc=cum_heads/torch.arange(1,N+1)
 head_frqc=head_frqc.numpy()
 
